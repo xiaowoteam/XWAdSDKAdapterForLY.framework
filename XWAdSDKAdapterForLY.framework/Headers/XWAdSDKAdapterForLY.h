a950bea96c8cca6e6fcd07953842a2c4e8a8fc7d
laole918 <laole918@qq.com>
2021-10-14 18:38:43 +0800

    first commit

---
+++ XWAdSDKAdapterForLY.framework/Headers/XWAdSDKAdapterForLY.h
@@ -0,0 +1,18 @@
+//
+//  XWAdSDKAdapterForLY.h
+//  XWAdSDKAdapterForLY
+//
+//  Created by laole918 on 2021/9/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XWAdSDKAdapterForLY.
+FOUNDATION_EXPORT double XWAdSDKAdapterForLYVersionNumber;
+
+//! Project version string for XWAdSDKAdapterForLY.
+FOUNDATION_EXPORT const unsigned char XWAdSDKAdapterForLYVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XWAdSDKAdapterForLY/PublicHeader.h>
+
+
